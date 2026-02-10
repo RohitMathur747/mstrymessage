03ae6bf1eded50bf89a07e5eb40bc55b4ffc10fd
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-02-10 23:03:22 +0530

    Next Js Connect mongo db

---
+++ src/Schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+  content: z
+    .string()
+    .min(10, { message: "Content must be atleast 10 Characters" })
+    .max(300, {
+      message: "Content should not be no longer than 300 Characters",
+    }),
+});
